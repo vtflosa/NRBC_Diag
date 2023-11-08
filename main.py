d5b85f722d9a09e15af0622d5c772db98ecd7339
vtf <florian.heissat@gmail.com>
2023-11-08 21:55:12 +0100

    First commit of main

---
+++ main.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+# -*- coding: Utf-8 -*
+""" This program is aimed to help emergency services to diagnose an NRBC disease from symptoms in a patient"""
+
+
+def main():
+    pass
+
+
+if __name__ == '__main__':
+    main()
